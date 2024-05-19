ab920a6319b8b67f3933c9ceec8afc7df622bb1b
sebamarambio96 <s.marambiosandoval@gmail.com>
2024-05-19 11:49:13 -0400

    fix sql

--- Sql coder.sql
+++ Sql coder.sql
@@ -5,7 +5,7 @@ CREATE TABLE `person` (
   `dni_verification_digit` integer,
   `age` integer,
   `email` varchar(255),
-  `created_at` timestamp
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `doctor` (
@@ -14,14 +14,14 @@ CREATE TABLE `doctor` (
   `specialty_id` integer,
   `email` VARCHAR(255),
   `phone_number` VARCHAR(20),
-  `created_at` TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `specialty` (
   `id` INTEGER PRIMARY KEY,
   `name` VARCHAR(255) UNIQUE,
   `description` TEXT,
-  `created_at` TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `user` (
@@ -30,24 +30,24 @@ CREATE TABLE `user` (
   `username` varchar(255),
   `pass` varchar(255),
   `role` varchar(255),
-  `created_at` timestamp
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `user_role` (
   `id` INTEGER PRIMARY KEY,
   `role_name` VARCHAR(255) UNIQUE,
   `description` TEXT,
-  `created_at` TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `session` (
   `id` integer PRIMARY KEY,
   `doctor_id` integer,
-  `start_time` timestamp,
-  `end_time` timestamp,
+  `start_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `end_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `confirmed_by_doctor` boolean DEFAULT false,
   `available` boolean DEFAULT false,
-  `created_at` timestamp
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `session_request` (
@@ -55,15 +55,15 @@ CREATE TABLE `session_request` (
   `person_id` integer,
   `session_id` integer,
   `confirmed_by_patient` boolean DEFAULT false,
-  `created_at` timestamp
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `session_record` (
   `id` integer PRIMARY KEY,
   `session_id` integer,
-  `session_date` TIMESTAMP,
+  `session_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `observations` TEXT,
-  `created_at` TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE `user` ADD FOREIGN KEY (`person_id`) REFERENCES `person` (`id`);
@@ -78,6 +78,6 @@ ALTER TABLE `user` ADD FOREIGN KEY (`role`) REFERENCES `user_role` (`role_name`)
 
 ALTER TABLE `session_record` ADD FOREIGN KEY (`session_id`) REFERENCES `session` (`id`);
 
-ALTER TABLE `person` ADD FOREIGN KEY (`id`) REFERENCES `session_request` (`person_id`);
+ALTER TABLE `session_request` ADD FOREIGN KEY (`person_id`) REFERENCES `person` (`id`);
 
 ALTER TABLE `session_request` ADD FOREIGN KEY (`session_id`) REFERENCES `session` (`id`);
